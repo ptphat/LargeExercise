94ed9f5911b4335bc5612a58879f7265fdfe4d07
ptphat <1612537@hcmut.edu.vn>
2017-04-16 10:13:40 +0700

    Fixed bug

--- LargeExercise/LargeExercise/Source.cpp
+++ LargeExercise/LargeExercise/Source.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 void main() {
 	string st, st_value;
-	float alpha = 0, a = 0, b = 0;
+	float alpha = 0, a = 0, a_starter=0, b = 0, b_starter=0;
 	int M = 0, K = 0;
 	getline(cin, st);
 	getline(cin, st);
@@ -17,9 +17,9 @@ void main() {
 	getline(cin, st); stringstream ss2(st);
 	ss2 >> st_value >> alpha;
 	getline(cin, st); stringstream ss3(st);
-	ss3 >> st_value >> a;
+	ss3 >> st_value >> a_starter;
 	getline(cin, st); stringstream ss4(st);
-	ss4 >> st_value >> b;
+	ss4 >> st_value >> b_starter;
 	getline(cin, st); stringstream ss5(st);
 	ss5 >> st_value >> K;
 	getline(cin, st); //bo qua ----
@@ -27,83 +27,74 @@ void main() {
 	getline(cin, st); //bo qua ----
 	double x[NUM_SAMPLES], t[NUM_SAMPLES], y[NUM_SAMPLES];
 	int length = 0;
-	for (int i = 1; i <= NUM_SAMPLES; i++) {
+	for (int i = 0; i < NUM_SAMPLES; i++) {
 		getline(cin, st); stringstream ss(st);
 		ss >> x[i] >> t[i];
 		if (x[i] < 0.01) {
-			length = i - 1; //tim ra so cap gia tri x, t
+			length = i; //tim ra so cap gia tri x, t
 			break;
 		}
 	}
+	cout << setprecision(3) << fixed;
+	cout << M << " " << alpha << " " << a << " " << b << " " << K << endl;
 	int D;
 	D = length / K;
-	cout << K << " "<< D << endl;
 	// K: so ngan, D: so gia tri trong 1 ngan, M: So lan lap, TST: chua mau ngan hien tai, TSN: chua mau cac ngan con lai
-	float TST_x[NUM_SAMPLES], TST_t[NUM_SAMPLES], TRN_x[NUM_SAMPLES], TRN_t[NUM_SAMPLES];
-	for (int j = 1; j <= K; j++) {
-		//Xet tung ngan
-		//Phan chia tap hop TST va TRN
-		// Voi ngan thu 1
-		if (j == 1) {
-			for (int i = 1; i <= 2 * D - 1; i++) {
-				TST_x[i] = x[i];
-				TST_t[i] = t[i];
-			}
-			for (int i = 2 * D; i <= length; i++) {
-				TRN_x[i] = x[i];
-				TRN_t[i] = t[i];
-			}
-		}
-		// Voi ngan cuoi
-		else if (j == K) {
-			for (int i = D*(K - 1); i <= length; i++) {
-				TST_x[i] = x[i];
-				TST_t[i] = t[i];
-			}
-			for (int i = 1; i < D*(K - 1); i++) {
-				TRN_x[i] = x[i];
-				TRN_t[i] = t[i];
-			}
-		}
-		// Voi cac ngan khac
-		else {
-			for (int i = 1; i <= length; i++) {
-				if (i >= D*(j - 1)&i <= D*j - 1) {
-					TST_x[i] = x[i];
-					TST_t[i] = t[i];
+	cout << K << " " << D << endl;
+	for (int i = 0; i < K; i++) {
+		if (i != K - 1) {
+			float gradient_a = 0, gradient_b = 0, y[NUM_SAMPLES] = { 0 };
+			for (int k = 0; k < M; k++) {
+				for (int j = 0; j < length; j++) {
+				if (j < D*i || j >= D*(i + 1)) {
+						y[j] = a*x[j] + b;
+						gradient_a = gradient_a + (y[j] - t[j])*x[j];
+						gradient_b = gradient_b + (y[j] - t[j]);
+					}
 				}
-				else {
-					TRN_x[i] = x[i];
-					TRN_t[i] = t[i];
-				}
-			}
-		}
-	}
-	for (int j =1; j<=K; j++){
-		// Giai thuat toi uu GD cho TRN
-		double gradient_a = 0, gradient_b = 0;
-		int temp = 0;
-		for (int i = 1; i <= M; i++) {
-			// Lap lai M lan
-			for (int k = 1; k <= length; k++) {
-				if (TRN_x[k] != 0) {
-					// Xet tap TRN
-					y[k] = a*TRN_x[k] + b;
-					gradient_a = gradient_a + (y[k] - TRN_t[k])*x[k]; // Phuong trinh 3
-					gradient_b = gradient_b + (y[k] - TRN_t[k]); // Phuong trinh 4
-					temp = k;
-				}
-			}
-			if (TRN_x[temp] != 0) {
-				// Xet tap TRN
 				a = a - alpha*gradient_a / len(gradient_a, gradient_b); // Phuong trinh 6 va chuan hoa
 				b = b - alpha*gradient_b / len(gradient_a, gradient_b); // Phuong trinh 7 va chuan hoa
 				gradient_a = gradient_b = 0;
 			}
+			cout << a << " " << b << endl;
 		}
-		cout << a << " " << b << endl;
+		else {
+			float gradient_a = 0, gradient_b = 0, y[NUM_SAMPLES] = { 0 };
+			for (int k = 0; k < M; k++) {
+				for (int j = 0; j < length; j++) {
+					if (j < D*(K-1)) {
+						y[j] = a*x[j] + b;
+						gradient_a = gradient_a + (y[j] - t[j])*x[j];
+						gradient_b = gradient_b + (y[j] - t[j]);
+					}
+				}
+				a = a - alpha*gradient_a / len(gradient_a, gradient_b); // Phuong trinh 6 va chuan hoa
+				b = b - alpha*gradient_b / len(gradient_a, gradient_b); // Phuong trinh 7 va chuan hoa
+				gradient_a = gradient_b = 0;
+			}
+			cout << a << " " << b << endl;
+		}
+		a = a_starter; b = b_starter;
 	}
-	
+}
+/*
+		// Xet TST
+		double E = 0, temp1 = 0, temp2 = 0, e_ngang = 0, sigma = 0, vmax = 0, vmin = 0;
+		for (int i = 1; i <= length; i++)
+			if (TST_x[i] != 0) {
+				temp1 = temp1 + pow((a*TST_x[i] + b - TST_t[i]), 2) / D; // Bien tam thoi phuong trinh 8
+				e_ngang = e_ngang + (a*TST_x[i] + b - t[i]) / D; // Tinh phuong trinh 12
+			}
+		for (int i = 1; i <= length; i++)
+			if (TST_x[i] != 0) temp2 = temp2 + pow((a*TST_x[i] + b - t[i] - e_ngang), 2); // Bien tam thoi phuong trinh 11
+		E = sqrt(temp1); // Tinh phuong trinh 8
+		sigma = sqrt(temp2); // Tinh phuong trinh 11
+		vmax = 3 * sigma;
+		vmin = -3 * sigma;
+
+		cout << right << setw(7) << a << setw(7) << b << setw(7) << E << setw(10) << sigma << endl;
+		a = b = gradient_a = gradient_b = 0;
+	}
 }
 
 
